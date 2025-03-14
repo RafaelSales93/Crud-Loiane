ad0edf07f44bf23aca57ca728109f22892a37404
RafaelSales93 <fco.rsales@gmail.com>
2025-03-14 15:01:02 -0300

    Atualizar Curso: API Spring HTTP PUT

--- Back/src/main/java/com/rafael/crud_spring/controller/CursosController.java
+++ Back/src/main/java/com/rafael/crud_spring/controller/CursosController.java
@@ -6,47 +6,57 @@ import com.rafael.crud_spring.repository.CursoRepository;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.rafael.crud_spring.model.Curso;
 
 import lombok.AllArgsConstructor;
 
-@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/cursos")
 @AllArgsConstructor
 public class CursosController {
-
+    
     private final CursoRepository cursoRepository;
 
-
     @GetMapping
-    public List<Curso> list(){
+    public @ResponseBody List<Curso> list() {
         return cursoRepository.findAll();
     }
-    
+
     @GetMapping("/{id}")
-    public ResponseEntity<Curso>findById(@PathVariable Long id){
+    public ResponseEntity<Curso> findById(@PathVariable Long id) {
         return cursoRepository.findById(id)
-        .map(record -> ResponseEntity.ok().body(record))
-        .orElse(ResponseEntity.notFound().build());
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @ResponseStatus(code = HttpStatus.CREATED)
     @PostMapping
-public Curso create(@RequestBody Curso curso){
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Curso create(@RequestBody Curso curso) {
 
-   return cursoRepository.save(curso);
-//    return ResponseEntity.status(HttpStatus.CREATED)
-//    .body(cursoRepository.save(curso));
+        return cursoRepository.save(curso);
+    }
 
-}
+    @PutMapping("/{id}")
+    public ResponseEntity<Curso> update(@PathVariable Long id,
+            @RequestBody Curso curso) {
+        return cursoRepository.findById(id)
+                .map(record -> {
+                    record.setNome(curso.getNome());
+                    record.setCategoria(curso.getCategoria());
+                    Curso update = cursoRepository.save(record);
+                    return ResponseEntity.ok().body(update);
+                })
+                .orElse(ResponseEntity.notFound().build());
+
+    }
+
 }
