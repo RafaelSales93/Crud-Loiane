fa8e169429526fc3257512b28f184a14fd6bd6d5
RafaelSales93 <fco.rsales@gmail.com>
2025-03-21 15:25:52 -0300

    Validando Enums com Java Bean Validations

--- Back/src/main/java/com/rafael/crud_spring/dto/CursoDTO.java
+++ Back/src/main/java/com/rafael/crud_spring/dto/CursoDTO.java
@@ -5,16 +5,17 @@ import java.util.List;
 import org.hibernate.validator.constraints.Length;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.rafael.crud_spring.enums.Categoria;
+import com.rafael.crud_spring.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CursoDTO(
     @JsonProperty("_id") Long id,
     @NotBlank @NotNull @Length(min = 5, max = 100) String nome,
-    @NotNull @Length(max = 10) @Pattern(regexp = "Front-End|Back-End") String categoria,
+    @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Categoria.class ) String categoria,
     @NotNull @NotEmpty @Valid List<LessonDTO> lessons ) {
 }
